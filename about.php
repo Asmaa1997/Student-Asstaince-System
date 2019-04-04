8ce8b39380b9dd0d9b47fe3063e6f3c3c3aa0db2
Asmaa <46787935+Asmaa1997@users.noreply.github.com>
2019-04-04 04:20:19 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,128 @@
+<?php 
+
+include_once("include/header.php");
+
+?>
+<nav class="navbar navbar-expand-lg navbar-light">
+            
+          <!-- <img src="images/Arab_Open_University_Logo.jpg" width="120" height="100" class="c1"  class="rounded float-right" class="img-fluid" class="img-thumbnail"style="margin-left:0%;margin-right:0%; padding-right:50px; padding-left:0px; ">-->
+        
+          <div class="hedder-up">
+         <h1 ><a href="index.php" class="navbar-brand" data-blast="color">SAS</a></h1>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="headdser-nav-color" data-blast="bgColor">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="about.php" class="nav-link scroll" >About</a>
+                </li>
+                <li class="nav-item">
+                  <a href="index.php" class="nav-link scroll">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a href="index.php" class="nav-link scroll">Galary</a>
+                </li>
+                <li class="nav-item">
+                  <a href="index.php" class="nav-link scroll">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+            <!--<img src="images/logo.png" width="100" height="100" class="c1"  class="rounded float-left" class="img-fluid" class="img-thumbnail" style="margin-left:0%;margin-left:0%; padding-left:50px; padding-left:30px; ">-->
+           </nav>
+        <!--//navigation section -->
+        <div class="clearfix"> </div>
+      </div>
+      <!--banner -->
+      <!-- Slideshow 4 -->
+         <div class="slider">
+        <div class="callbacks_container">
+          <ul class="rslides" id="slider4">
+            <li>
+              <div class="slider-img one-img">
+                <div class="container">
+                  <div class="slider-info text-left">
+                    <h5 >Student Assistance System</h5>
+                    <div class="outs_more-buttn" >
+                      <a href="#" data-toggle="modal" data-target="#exampleModalLive" data-blast="bgColor">More</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="slider-img two-img">
+                <div class="container">
+                  <div class="slider-info text-left">
+                  <h5 >Student Assistance System</h5>
+                    <div class="outs_more-buttn">
+                      <a href="#" data-toggle="modal" data-target="#exampleModalLive" data-blast="bgColor">More</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="slider-img three-img">
+                <div class="container">
+                  <div class="slider-info text-left">
+                  <h5 >Student Assistance System</h5>
+                    <div class="outs_more-buttn">
+                      <a href="#" data-toggle="modal" data-target="#exampleModalLive" data-blast="bgColor">More</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- This is here just to demonstrate the callbacks -->
+        <!-- <ul class="events">
+          <li>Example 4 callback events</li>
+          </ul>-->
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <!-- //banner -->
+    <!--about-->
+    <!-- //banner -->
+    <!--about-->
+  <section class="about" id="about">
+      <div class="container py-lg-5 py-md-5 py-sm-4 py-3">
+        <!--Horizontal Tab-->
+        <div id="horizontalTab">
+          <ul class="resp-tabs-list justify-content-center">
+            <li data-blast="bgColor">Student Union</li>
+            </ul>
+            <div class="tab3">
+              <div class="row mt-lg-4 mt-3">
+                <div class="col-md-7 latest-list">
+                  <div class="about-jewel-agile-left">
+                    <h4 class="mb-3" data-blast="color">Who we are</h4>
+                    <p>As a result of the alliance and cooperation between the university and the student, the so-called Student Union Council was established, which seeks to consolidate the principles of cooperation and interaction between the university and the students.
+                    The role and goal of the student council are to create an inherent relationship between the student and the university and to seek to practice and give the student the right to freedom of expression of his views and requirements within the regulations and regulations set by the university.</p>
+                   <!-- <h5 data-blast="color">Who we are</h5>-->
+                  </div>
+                </div>
+                <div class="col-md-5 about-txt-img">
+                  <img src="images/logo.png" width="70%" class="img-thumbnail" alt="">
+                </div>
+              </div>
+            </div>
+           
+          </div>
+        </div>
+    </section>
+
+
+<?php
+
+include_once("include/footer.php");
+
+?>
